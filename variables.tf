86509805d824c10337ed5057be5f83530394547f
SMRUTI RANJAN PATRA <151839758+patrsmr@users.noreply.github.com>
2024-04-04 22:30:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+# variables.tf
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default = "192.170.0.0/20" # Default value provided, can be changed
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"] # List of AZs, adjust as needed
+}
 

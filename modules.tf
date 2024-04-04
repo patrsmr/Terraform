051a95c5424c3fce965de003e1ca581b2b07c547
SMRUTI RANJAN PATRA <151839758+patrsmr@users.noreply.github.com>
2024-04-04 22:40:49 +0530

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -1,1 +1,12 @@
+# modules.tf
+
+module "base_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  namespace = 11
+  stage     = var.stage
+  name      = var.name
+  attributes = ["subnet"]
+}
 
